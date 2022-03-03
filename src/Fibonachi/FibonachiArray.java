88a91c47528acac413659af20b1344eed2057548
Timur12rus <tim.gordeev2015@yandex.ru>
2022-03-03 13:49:28 +0300

    исправил FibonachiArray для наглядности

--- src/Fibonachi/FibonachiArray.java
+++ src/Fibonachi/FibonachiArray.java
@@ -2,16 +2,16 @@ package Fibonachi;
 
 public class FibonachiArray {
     public static void main(String[] args) {
-        System.out.println(fib(10));   // (индексация чисел начинается с "0", n - порядковый номер числа в последовательности
+        System.out.println(fib(10));   // (порядковый номер в последовательности, не индекс!    }
     }
 
     private static int fib(int n) {
         int[] array = new int[n];
         array[0] = 0;
         array[1] = 1;
-        for (int i = 2; i < n; i++) {
+        for (int i = 2; i < array.length; i++) {
             array[i] = array[i - 1] + array[i - 2];
         }
-        return array[n - 1];
+        return array[array.length - 1];
     }
 }
